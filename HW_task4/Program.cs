3b3e3c64a297fd5038f608a8cd83e2b1413bd8be
Ivan_Ezgurov <ezgurovivan@gmail.com>
2023-03-02 19:03:37 +0300

    HW_task4.1

--- HW_task4/Program.cs
+++ HW_task4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход
+// три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if( b > max )
+{
+max = a;
+}
+if( c > max)
+{
+    max = c;
+}
+Console.WriteLine($"Наибольшее число: {max}");
+
